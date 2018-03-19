a951c8d9819a7bba6bf9a4648c4d1be7496800f7
xinyihaha <2977728756@qq.com>
2018-03-19 09:15:00 +0800

    175 second exam

--- app/src/main/java/com/example/sxy/firstandroid/MainActivity.java
+++ app/src/main/java/com/example/sxy/firstandroid/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.sxy.firstandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.*;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+public class MainActivity extends Activity{
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            Log.d("MainActivity","oncreate");
+            // The activity is being created.
+        }
+        @Override
+        protected void onStart() {
+            super.onStart();
+            Log.d("MainActivity","onstart");
+            // The activity is about to become visible.
+        }
+        @Override
+        protected void onResume() {
+            super.onResume();
+            Log.d("MainActivity","onresume");
+            // The activity has become visible (it is now "resumed").
+        }
+        @Override
+        protected void onPause() {
+            super.onPause();
+            Log.d("MainActivity","onpause");
+            // Another activity is taking focus (this activity is about to be "paused").
+        }
+        @Override
+        protected void onStop() {
+            super.onStop();
+            Log.d("MainActivity","onstop");
+            // The activity is no longer visible (it is now "stopped")
+        }
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            Log.d("MainActivity","ondestroy");
+            // The activity is about to be destroyed.
+        }
     }
-}
